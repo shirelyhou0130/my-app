309f62045bbc57b5463c3b4a1b6aa47081e41409
shirelyhou0130 <h2772489816@163.com>
2022-06-17 00:09:00 +0800

    add handleClick

--- src/index.js
+++ src/index.js
@@ -20,6 +20,11 @@ class Board extends React.Component{
       squares: Array(9).fill(null),
     }
   }
+  handleClick(i){
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
   renderSquare(i){
     return <Square 
               value={this.state.squares[i]} 
