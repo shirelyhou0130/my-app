4e018881dc158f18d8d4d3a610a4a4a535dfd0af
shirelyhou0130 <h2772489816@163.com>
2022-06-17 00:16:32 +0800

    mark 'O' on the board

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ class Board extends React.Component{
   }
   handleClick(i){
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
       xIsNext: !this.state.xIsNext,
