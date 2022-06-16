8f766dd31c9df9df195ec71d0a02b90482fb9ae5
shirelyhou0130 <h2772489816@163.com>
2022-06-17 00:14:46 +0800

    mark 'O' on the board

--- src/index.js
+++ src/index.js
@@ -25,12 +25,16 @@ class Board extends React.Component{
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     }
   }
   handleClick(i){
     const squares = this.state.squares.slice();
     squares[i] = 'X';
-    this.setState({squares: squares});
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
   renderSquare(i){
     return <Square 
@@ -39,7 +43,7 @@ class Board extends React.Component{
             />;
   }
   render(){
-    const status = 'Next player: X';
+    const status = 'Next player: +'(this.state.xIsNext ? 'X' : 'O');
 
     return(
       <div>
